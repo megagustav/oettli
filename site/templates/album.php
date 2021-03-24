bcc94c1ed6f208a6e7fa650510582901ddd1585b
Gustav Moorhouse <g.moorhouse@me.com>
2021-03-24 10:58:34 +0000

    added arena project

--- site/templates/album.php
+++ site/templates/album.php
@@ -19,29 +19,5 @@
 */
 ?>
 <?php snippet('header') ?>
-<article>
-  <?php // snippet('intro') ?>
-  <div class="grid">
-
-    <div class="column" style="--columns: 4">
-      <div class="text">
-        <?= $page->text() ?>
-      </div>
-    </div>
-
-    <div class="column" style="--columns: 8">
-      <ul class="album-gallery">
-        <?php foreach ($gallery as $image): ?>
-        <li>
-          <a href="<?= $image->url() ?>" data-lightbox>
-            <figure class="img" style="--w:<?= $image->width() ?>;--h:<?= $image->height() ?>">
-              <?= $image->resize(800) ?>
-            </figure>
-          </a>
-        </li>
-        <?php endforeach ?>
-      </ul>
-    </div>
-
-</article>
+<?php snippet('layouts', ['field' => $page->layout()])  ?>
 <?php snippet('footer') ?>
--- site/templates/default.php
+++ site/templates/default.php
@@ -20,11 +20,6 @@
 ?>
 <?php snippet('header') ?>
 
-<article>
-  <h1 class="h1"><?= $page->title()->html() ?></h1>
-  <div class="text">
-    <?= $page->text()->kt() ?>
-  </div>
-</article>
+<?php snippet('layouts', ['field' => $page->layout()])  ?>
 
 <?php snippet('footer') ?>
